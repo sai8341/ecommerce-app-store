7c1607f24ce1b825edc0ede82441791af373b011
Sai <saikumar19123@gmail.com>
2024-02-05 15:41:44 +0530

    components deleted

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Shop from './Components/Pages/Shop';
-import ShopCategory from './Components/Pages/ShopCategory';
-import Product from './Components/Pages/Product';
-import Cart from './Components/Pages/Cart';
-import LoginSignup from './Components/Pages/LoginSignup';
 
-function App() {
+
+const App = () => {
   return (
     <div>
-      <Navbar />
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Shop />} />
-        <Route path='/men' element={<ShopCategory category={"men"} />} />
-        <Route path='/women' element={<ShopCategory category={"women"} />} />
-        <Route path='/kids' element={<ShopCategory category={"kid"} />} />
-        <Route path='/product' element={<Product />}>
-          <Route path=':productId' element={<Product />} />
-        </Route>
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/login' element={<LoginSignup />} />
-      </Routes>
-      </BrowserRouter>
+      This is App
     </div>
   );
 }
