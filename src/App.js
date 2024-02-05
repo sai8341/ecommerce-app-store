6d420cd5018817bda3ece805df219e1669b7c2b6
Sai <saikumar19123@gmail.com>
2024-02-05 17:37:05 +0530

    Pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Shop from './Components/Pages/Shop';
+import Navbar from './Components/Navbar/Navbar';
+import ShopCategory from './Components/Pages/ShopCategory';
+import Product from './Components/Pages/Product';
+import Cart from './Components/Pages/Cart';
+import LoginSignup from './Components/Pages/LoginSignup';
 
 const App = () => {
   return (
-    <div>
-      This is App
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Shop />} />
+      <Route path='/mens' element={<ShopCategory />} />
+      <Route path='/womens' element={<ShopCategory /> } />
+      <Route path='/kids' element={<ShopCategory />}/>
+      <Route path='/product' element={<Product />}>
+        <Route path=':productId' element={<Product />} />
+      </Route>
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/login' element={<LoginSignup />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+
+import { Link } from 'react-router-dom'
+
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+
+const Navbar = () => {
+    const [menu, setMenu] = useState("shop");
+
+    console.log(menu)
+
+    return (
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>SHOPPER</p>
+            </div>
+
+            <ul className="nav-menu">
+
+                <li onClick={() => setMenu("shop")}>
+                    <Link style={{textDecoration: 'none', color: 'color: #626262'}} to='/'>Shop</Link> 
+                    { menu === 'shop' ? <hr /> : <React.Fragment /> }
+                </li>
+
+                <li onClick={() => setMenu('men')}>
+                    <Link style={{textDecoration: 'none'}} to='/mens'>Men</Link> 
+                    { menu === 'men' ? <hr /> : <React.Fragment /> } 
+                </li>
+
+                <li onClick={() => setMenu('womens')}>
+                    <Link style={{textDecoration: 'none'}} to='/women'>Women</Link> 
+                    { menu === 'women' ? <hr /> : <React.Fragment /> }
+                </li>
+                <li onClick={() => setMenu('kids')}>
+                    <Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link>
+                    { menu === 'kids' ? <hr /> : <React.Fragment /> } 
+                </li>
+            </ul>
+
+            <div className="nav-login-cart">
+                <Link to='/login'><button>Login</button></Link>
+
+                <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+                <div className='nav-cart-count'>0</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
